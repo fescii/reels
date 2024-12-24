c3dab1df4baa3600415cf4b68e5743e854584de9
fescii <isfescii@gmail.com>
2024-12-24 15:29:14 +0300

    Add tab navigation and number formatting to ChatsContainer component

--- src/chats/chats.js
+++ src/chats/chats.js
@@ -12,6 +12,41 @@ export default class ChatsContainer extends HTMLElement {
   connectedCallback() {
   }
 
+  formatNumber = numStr => {
+    try {
+      const num = parseInt(numStr);
+
+      // less than a thousand: return the number
+      if (num < 1000) return num;
+
+      // less than a 10,000: return the number with a k with two decimal places
+      if (num < 10000) return `${(num / 1000).toFixed(2)}k`;
+
+      // less than a 100,000: return the number with a k with one decimal place
+      if (num < 100000) return `${(num / 1000).toFixed(1)}k`;
+
+      // less than a million: return the number with a k with no decimal places
+      if (num < 1000000) return `${Math.floor(num / 1000)}k`;
+
+      // less than a 10 million: return the number with an m with two decimal places
+      if (num < 10000000) return `${(num / 1000000).toFixed(2)}M`;
+
+      // less than a 100 million: return the number with an m with one decimal place
+      if (num < 100000000) return `${(num / 1000000).toFixed(1)}M`;
+
+      // less than a billion: return the number with an m with no decimal places
+      if (num < 1000000000) return `${Math.floor(num / 1000000)}M`;
+
+      // a billion or more: return the number with a B+
+      if (num >= 1000000000) return `${Math.floor(num / 1000000000)}B+`;
+
+      // else return the zero
+      return '0';
+    } catch (error) {
+      return '0';
+    }
+  }
+
   getTemplate() {
     return /* html */`
       ${this.getBody()}
@@ -23,6 +58,7 @@ export default class ChatsContainer extends HTMLElement {
     return /* html */`
       <div class="chats">
         ${this.getForm()}
+        ${this.getTab()}
       </div>
       <div class="people">People</div>
     `;
@@ -37,13 +73,48 @@ export default class ChatsContainer extends HTMLElement {
         <div class="contents">
           <input type="text" name="q" id="query" placeholder="What's your query?">
           <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <circle cx="11.7666" cy="11.7667" r="8.98856" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"  stroke-linejoin="round" />
-            <path d="M18.0183 18.4853L21.5423 22.0001" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+            <circle cx="11.7666" cy="11.7667" r="8.98856" stroke="currentColor" stroke-width="1.8" stroke-linecap="round"  stroke-linejoin="round" />
+            <path d="M18.0183 18.4853L21.5423 22.0001" stroke="currentColor" stroke-width="1.8" stroke-linecap="round" stroke-linejoin="round" />
           </svg>
           <button type="submit">Search</button>
         </div>
       </form>
     `
+  }
+
+  getTab = () => {
+    return /* html */`
+      <ul class="tabs">
+        <li class="tab">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" color="currentColor" fill="none">
+            <path d="M12.5 3H11.5C7.02166 3 4.78249 3 3.39124 4.39124C2 5.78249 2 8.02166 2 12.5C2 16.9783 2 19.2175 3.39124 20.6088C4.78249 22 7.02166 22 11.5 22C15.9783 22 18.2175 22 19.6088 20.6088C21 19.2175 21 16.9783 21 12.5V11.5" stroke="currentColor" stroke-width="1.8" stroke-linecap="round" />
+            <path d="M22 5.5C22 7.433 20.433 9 18.5 9C16.567 9 15 7.433 15 5.5C15 3.567 16.567 2 18.5 2C20.433 2 22 3.567 22 5.5Z" stroke="currentColor" stroke-width="1.8" />
+            <path d="M7 11H11" stroke="currentColor" stroke-width="1.8" stroke-linecap="round" stroke-linejoin="round" />
+            <path d="M7 16H15" stroke="currentColor" stroke-width="1.8" stroke-linecap="round" stroke-linejoin="round" />
+          </svg>
+          <span class="text">All</span>
+          <span class="count">${this.formatNumber(this.getAttribute("all"))}</span>
+        </li>
+        <li class="tab active">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" color="currentColor" fill="none">
+            <path d="M21.9598 10.9707C22.0134 11.8009 22.0134 12.6607 21.9598 13.4909C21.6856 17.7332 18.3536 21.1125 14.1706 21.3905C12.7435 21.4854 11.2536 21.4852 9.8294 21.3905C9.33896 21.3579 8.8044 21.2409 8.34401 21.0513C7.83177 20.8403 7.5756 20.7348 7.44544 20.7508C7.31527 20.7668 7.1264 20.9061 6.74868 21.1846C6.08268 21.6757 5.24367 22.0285 3.99943 21.9982C3.37026 21.9829 3.05568 21.9752 2.91484 21.7351C2.77401 21.495 2.94941 21.1626 3.30021 20.4978C3.78674 19.5758 4.09501 18.5203 3.62791 17.6746C2.82343 16.4666 2.1401 15.036 2.04024 13.4909C1.98659 12.6607 1.98659 11.8009 2.04024 10.9707C2.31441 6.72838 5.64639 3.34913 9.8294 3.07107C11.0318 2.99114 11.2812 2.97856 12.5 3.03368" stroke="currentColor" stroke-width="1.8" stroke-linecap="round" stroke-linejoin="round" />
+            <path d="M8.5 15H15.5M8.5 10H12" stroke="currentColor" stroke-width="1.8" stroke-linecap="round" stroke-linejoin="round" />
+            <path d="M22 5.5C22 7.433 20.433 9 18.5 9C16.567 9 15 7.433 15 5.5C15 3.567 16.567 2 18.5 2C20.433 2 22 3.567 22 5.5Z" stroke="currentColor" stroke-width="1.8" />
+          </svg>
+          <span class="text">Unread</span>
+          <span class="count">${this.formatNumber(43)}</span>
+        </li>
+        <li class="tab">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" color="currentColor" fill="none">
+            <path d="M21.7109 9.3871C21.8404 9.895 21.9249 10.4215 21.9598 10.9621C22.0134 11.7929 22.0134 12.6533 21.9598 13.4842C21.6856 17.7299 18.3536 21.1118 14.1706 21.3901C12.7435 21.485 11.2536 21.4848 9.8294 21.3901C9.33896 21.3574 8.8044 21.2403 8.34401 21.0505C7.83177 20.8394 7.5756 20.7338 7.44544 20.7498C7.31527 20.7659 7.1264 20.9052 6.74868 21.184C6.08268 21.6755 5.24367 22.0285 3.99943 21.9982C3.37026 21.9829 3.05568 21.9752 2.91484 21.7349C2.77401 21.4946 2.94941 21.1619 3.30021 20.4966C3.78674 19.5739 4.09501 18.5176 3.62791 17.6712C2.82343 16.4623 2.1401 15.0305 2.04024 13.4842C1.98659 12.6533 1.98659 11.7929 2.04024 10.9621C2.31441 6.71638 5.64639 3.33448 9.8294 3.05621C10.2156 3.03051 10.6067 3.01177 11 3" stroke="currentColor" stroke-width="1.8" stroke-linecap="round" stroke-linejoin="round" />
+            <path d="M8.5 15H15.5M8.5 10H12" stroke="currentColor" stroke-width="1.8" stroke-linecap="round" stroke-linejoin="round" />
+            <path d="M14 4.5L22 4.5M14 4.5C14 3.79977 15.9943 2.49153 16.5 2M14 4.5C14 5.20023 15.9943 6.50847 16.5 7" stroke="currentColor" stroke-width="1.8" stroke-linecap="round" stroke-linejoin="round" />
+          </svg>
+          <span class="text">Requests</span>
+          <span class="count">${this.formatNumber(3)}</span>
+        </li>
+      </ul>
+    `;
   }
 
   getStyles = () => {
@@ -202,6 +273,98 @@ export default class ChatsContainer extends HTMLElement {
           border-radius: 50px;
           -webkit-border-radius: 50px;
           -moz-border-radius: 50px;
+        }
+
+        ul.tabs {
+          border-bottom: var(--border);
+          display: flex;
+          z-index: 1;
+          flex-flow: row;
+          gap: 15px;
+          padding: 18px 0 10px;
+          margin: 0;
+          width: 100%;
+          list-style: none;
+          overflow-x: auto;
+          overflow-y: hidden;
+          white-space: nowrap;
+          scrollbar-width: 0;
+          -ms-overflow-style: none;
+          z-index: 1;
+          position: sticky;
+          top: 55px;
+          background: var(--background);
+        }
+
+        ul.tabs::-webkit-scrollbar {
+          display: none;
+          visibility: hidden;
+        }
+
+        ul.tabs > li.tab {
+          display: flex;
+          flex-flow: row;
+          align-items: center;
+          gap: 5px;
+          padding: 5px 0;
+          border-radius: 12px;
+          /*background: var(--gray-background);*/
+          color: var(--text-color);
+          font-family: var(--font-main), sans-serif;
+          font-size: 0.95rem;
+          font-weight: 500;
+          cursor: pointer;
+          transition: 0.3s;
+        }
+
+        ul.tabs > li.tab > span.count,
+        ul.tabs > li.tab > svg {
+          display: none;
+        }
+
+        ul.tabs > li.tab.active {
+          background: var(--tab-background);
+          padding: 5px 10px;
+          color: var(--text-color);
+        }
+
+        ul.tabs > li.tab.active > span.count,
+        ul.tabs > li.tab.active > svg,
+        ul.tabs > li.tab:not(.active):hover > span.count,
+        ul.tabs > li.tab:not(.active):hover > svg {
+          display: flex;
+        }
+
+        /* style hover tab: but don't touch tab with active class */
+        ul.tabs > li.tab:not(.active):hover {
+          background: var(--tab-background);
+          padding: 5px 10px;
+          color: var(--text-color);
+        }
+
+        ul.tabs > li.tab > svg {
+          width: 19px;
+          height: 19px;
+        }
+
+        ul.tabs > li.tab > .text {
+          font-size: 1rem;
+          padding: 0 5px 0 0;
+          font-weight: 500;
+        }
+
+        ul.tabs > li.tab > .count {
+          font-size: 0.85rem;
+          display: none;
+          align-items: center;
+          justify-content: center;
+          text-align: center;
+          font-weight: 500;
+          background: var(--accent-linear);
+          font-family: var(--font-text), sans-serif;
+          color: var(--white-color);
+          padding: 1px 7px 2px;
+          border-radius: 10px;
         }
 
         @media screen and (max-width: 660px) {

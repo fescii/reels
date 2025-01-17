d021afb0540e083311d3f2e04de5126cf0b8c4aa
fescii <isfescii@gmail.com>
2025-01-17 18:28:58 +0300

    Refactor controllers and routes: remove unused stats controller and edit topic route; update meta tags for improved SEO; enhance service worker for better caching and error handling; implement theme preference management in app.js

--- public/js/apps/apps.js
+++ public/js/apps/apps.js
@@ -1,8 +1,10 @@
 import shots from "./shots/index.js";
 import chats from "./chats/index.js";
+import uis from "./uis/index.js";
 
 export default function apps(text) {
   shots();
   chats();
+  uis();
   console.log(text);
 }
